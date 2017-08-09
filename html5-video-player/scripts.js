64a737710c4ab27be4b4db057b45621f1d8f2472
abohannon <abo46n2@gmail.com>
2017-08-09 15:01:15 -0700

    Completed HTML5 video player

---
+++ html5-video-player/scripts.js
@@ -0,0 +1,71 @@
+/* our elements */
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+/* our functions */
+
+function togglePlay() {
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+  // parseFloat turns the string from this.dataset.skip to a number
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = video.currentTime / video.duration * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  console.log(e.offsetX);
+
+  video.currentTime = scrubTime;
+}
+
+/* our event listeners */
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => {
+  button.addEventListener('click', skip);
+});
+
+ranges.forEach(slider => {
+  slider.addEventListener('change', handleRangeUpdate);
+});
+
+let mousedown = false;
+
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
